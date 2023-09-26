f06cb904701670df4d7ecf063e5d53e4bd781bea
MichaelNchor <manchor127@gmail.com>
2023-09-26 15:57:39 +0000

    initial full commit

---
+++ EcommerceAPI/Models/EcommerceAPIContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace EcommerceAPI.Models
+{
+    public partial class EcommerceAPIContext : DbContext
+    {
+        public EcommerceAPIContext()
+        {
+        }
+
+        public EcommerceAPIContext(DbContextOptions<EcommerceAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cart> Cart { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-K5527T2\\SQLEXPRESS;Database=EcommerceAPI;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cart>(entity =>
+            {
+                entity.HasKey(e => e.ItemId)
+                    .HasName("PK1")
+                    .IsClustered(false);
+
+                entity.Property(e => e.ItemId).HasColumnName("ItemID");
+
+                entity.Property(e => e.ItemName)
+                    .IsRequired()
+                    .HasMaxLength(400);
+
+                entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
