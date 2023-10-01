24fb638590f0dc62b4b39fb80ed3591fd543f04c
MichaelNchor <manchor127@gmail.com>
2023-10-01 18:45:36 +0000

    xUnit test setup

--- EcommerceAPI.Tests/ProductTests.cs
+++ EcommerceAPI.Tests/ProductTests.cs
@@ -1,17 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xunit;
 
 namespace EcommerceAPI.Tests
 {
-    [TestClass]
-    public class ProductTests
+    public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void Test1()
         {
 
         }
